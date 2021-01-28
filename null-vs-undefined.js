2915c13c2b3f28621304772304bbb716e9906482
mjrafi01 <dreamboyrafi16@gmail.com>
2021-01-28 00:50:06 -0800

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,15 @@
+// let pakhi;
+// console.log(pakhi);//undefined
+
+// function add(num1,num2) {
+//     console.log(num1+num2)
+// }
+
+// const result= add(12,13);
+// console.log(result);//undefined
+
+// const premik={name:'smart dude',phone:3455};
+// console.log(premik.gf)//undefined
+
+let fun=undefined;
+console.log(fun);
