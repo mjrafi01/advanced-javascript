8e1181aa9bb9769646eea62133f20ddf856e75dd
mjrafi01 <dreamboyrafi16@gmail.com>
2021-01-28 01:10:44 -0800

    double vs triple_equal

---
+++ double-vs-triple_equal.js
@@ -0,0 +1,44 @@
+// const first =2;
+// const second=3;
+
+// if (first==second) {//false
+//     console.log("IT is true")
+// } else {
+//     console.log("IT is false")
+// }
+
+// const first =2;
+// const second='2';
+//double equal check only value
+// if (first==second) {//true type is number,type is string 
+//     console.log("IT is true")
+// } else {
+//     console.log("IT is false")
+// }
+
+// const first =2;
+// const second='2';
+
+// if (first===second) {//false, triple equal will check strickly,check type also
+//     console.log("IT is true")
+// } else {
+//     console.log("IT is false")
+// }
+
+// const first =1;
+// const second=true;
+// //here 1 means true and 0 means false
+// if (first==second) {//true
+//     console.log("IT is true")
+// } else {
+//     console.log("IT is false")
+// }
+
+const first =0;
+const second=false;
+//here both are different type bcz triple equal check type also
+if (first===second) {//false
+    console.log("IT is true")
+} else {
+    console.log("IT is false")
+}
