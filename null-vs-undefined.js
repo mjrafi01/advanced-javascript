68a4d2d1d55170b62e93073cbf301fa8648b589d
mjrafi01 <dreamboyrafi16@gmail.com>
2021-01-28 01:12:43 -0800

    double vs triple_equal

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,15 +1,20 @@
-// let pakhi;
-// console.log(pakhi);//undefined
+let pakhi;
+console.log(pakhi);//undefined
 
-// function add(num1,num2) {
-//     console.log(num1+num2)
-// }
+function add(num1,num2) {
+    console.log(num1+num2)
+}
 
-// const result= add(12,13);
-// console.log(result);//undefined
+const result= add(12,13);
+console.log(result);//undefined
 
-// const premik={name:'smart dude',phone:3455};
-// console.log(premik.gf)//undefined
+const premik={name:'smart dude',phone:3455};
+console.log(premik.gf)//undefined
 
 let fun=undefined;
-console.log(fun);
+console.log(fun);//undefined
+
+
+
+//NULL means empty
+
