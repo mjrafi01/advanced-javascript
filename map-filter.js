2a3df3c0bbc65ac71b4c1433c174a0212dedc6ba
mjrafi01 <dreamboyrafi16@gmail.com>
2021-01-28 01:40:19 -0800

    map-filter.js

---
+++ map-filter.js
@@ -0,0 +1,35 @@
+const numbers=[3,4,5,3,6,7];
+// const output=[];
+// for (let index = 0; index < numbers.length; index++) {
+//     const element = numbers[index];
+//    const result =element*element;
+//    output.push(result); 
+// } console.log(output);
+
+// const square = element => element* element;
+// const square = x=>x*x;//both are arrow function
+
+
+//Using of map
+// let result=numbers.map(function(element,index,array) {
+//     console.log(element,index,array);
+//     return element*element;
+
+// })
+
+
+//map an arrow Function together
+
+// const result=numbers.map(x=>x*x);
+
+
+// console.log(result);
+
+//filter will help to get seelected items
+// const bigger =numbers.filter(x => x>5);
+// console.log(bigger);//greater than 5
+
+//find is similar as filter only different in return
+
+const isThere =numbers.find(x=> x>5);
+console.log(isThere);//find return only one element, answer 6
